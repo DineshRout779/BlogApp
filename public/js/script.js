93db19fec3ce25cad265bae8d74bfd8378e4a1b9
DineshRout779 <rddinesh779@gmail.com>
2021-05-03 18:59:19 +0530

    added add button

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,8 @@ const sideBar = document.querySelector(".lg-hide");
 const closeAlert = document.querySelector(".close-alert");
 const alert = document.querySelector(".alert");
 const preView = document.querySelectorAll(".blog-body p");
+const addBtn = document.querySelector(".add-post");
+const addPost = document.querySelector(".add");
 const maxLen = 200;
 
 // shorten the blog preview text
@@ -10,10 +12,15 @@ for (let i = 0; i < preView.length; i++) {
   preView[i].textContent = preView[i].textContent.slice(0, maxLen) + "...";
 }
 
-prev_text = menu.addEventListener("click", () => {
+menu.addEventListener("click", () => {
   sideBar.classList.toggle("sm-show");
 });
 
 closeAlert.addEventListener("click", () => {
   alert.style.display = "none";
+});
+
+addBtn.addEventListener("click", () => {
+  console.log("hello");
+  // addPost.classList.toggle("open");
 });
