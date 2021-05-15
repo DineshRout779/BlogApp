0ae740d0bb62e9ca93c1d51d17b340710a4d67c4
DineshRout779 <rddinesh779@gmail.com>
2021-05-15 10:45:55 +0530

    deleted sidebar

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,6 @@
-const menu = document.querySelector(".menu");
-const sideBar = document.querySelector(".lg-hide");
 const closeAlert = document.querySelector(".close-alert");
 const alert = document.querySelector(".alert");
 const preView = document.querySelectorAll(".blog-body p");
-const addBtn = document.querySelector(".add-post");
-const addPost = document.querySelector(".add");
 const maxLen = 200;
 
 // shorten the blog preview text
@@ -12,15 +8,6 @@ for (let i = 0; i < preView.length; i++) {
   preView[i].textContent = preView[i].textContent.slice(0, maxLen) + "...";
 }
 
-menu.addEventListener("click", () => {
-  sideBar.classList.toggle("sm-show");
-});
-
 closeAlert.addEventListener("click", () => {
   alert.style.display = "none";
-});
-
-addBtn.addEventListener("click", () => {
-  console.log("hello");
-  // addPost.classList.toggle("open");
 });
