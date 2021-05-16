895bf8433ed1f908b8c80cf02b430f5936a544e0
DineshRout779 <rddinesh779@gmail.com>
2021-05-16 20:33:25 +0530

    minor changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,4 @@
 const closeAlert = document.querySelector(".close-alert");
 const alert = document.querySelector(".alert");
-const preView = document.querySelectorAll(".blog-body p");
-const maxLen = 200;
-
-// shorten the blog preview text
-for (let i = 0; i < preView.length; i++) {
-  preView[i].textContent = preView[i].textContent.slice(0, maxLen) + "...";
-}
 
 closeAlert.addEventListener("click", () => (alert.style.display = "none"));
