7caabb6148df637b948035e3d3ccaa4d37b36ba3
DineshRout779 <rddinesh779@gmail.com>
2021-07-15 12:18:33 +0530

    added seqeulize ORM

--- app.js
+++ app.js
@@ -1,44 +1,41 @@
 const express = require("express");
-const mysql = require("mysql");
 const path = require("path");
 const app = express();
-const dotenv = require("dotenv");
-const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const indexRouter = require("./routes/pages");
+const indexRouter = require("./routes/index");
 const authRouter = require("./routes/auth");
+const postRouter = require("./routes/post");
+const db = require("./models");
+const methodOverride = require("method-override");
 
 app.use(
-  bodyParser.urlencoded({
+  express.urlencoded({
     extended: false,
   })
 );
-app.use(bodyParser.json());
+
+app.use(methodOverride("_method"));
+
+app.use(express.json());
 
 app.use(cookieParser());
-
-dotenv.config({ path: "./.env" });
-
-const db = mysql.createConnection({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE,
-});
-
-db.connect((err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Database connected!");
-  }
-});
 
 app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
+
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log("Drop and re-sync db.");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.use("/", indexRouter);
 app.use("/auth", authRouter);
+app.use("/posts", postRouter);
 
 const PORT = process.env.PORT || 3000;
 
