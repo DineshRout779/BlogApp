0df5aebf0318c9d3f73d5d748f126e8439e99b5d
Dinesh Rout <rddinesh779@gmail.com>
2021-07-20 10:55:23 +0530

    added user controller for user profile

---
+++ controllers/user.js
@@ -0,0 +1,8 @@
+// show user profile
+exports.viewProfile = async (req, res) => {};
+
+// update profile
+exports.updateProfile = async (req, res) => {};
+
+// delete profile
+exports.deleteProfile = async (req, res) => {};
