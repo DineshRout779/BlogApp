62d746cb35b844935a393a14b0e9c426accc7dc6
Dinesh Rout <rddinesh779@gmail.com>
2021-07-25 13:52:00 +0530

    refactor - deleted unnecessary code

--- routes/index.js
+++ routes/index.js
@@ -1,59 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const index = require("../controllers");
-const { verifyToken } = require("../middlewares/verifyToken");
 
 router.get("/", index.index);
 router.get("/login", index.login);
 router.get("/register", index.register);
-
-router.get("/delete/:userId/:blogId", verifyToken, (req, res) => {
-  const { userId, blogId } = req.params;
-
-  db.query("DELETE FROM posts WHERE id= ?", [blogId], (error, results) => {
-    if (error) {
-      return console.log(error);
-    }
-    console.log("post deleted!");
-    res.redirect(`/dashboard/${userId}`);
-  });
-});
-
-router.get("/edit/:userId/:blogId", (req, res) => {
-  const { blogId, userId } = req.params;
-
-  db.query("SELECT * FROM posts WHERE id=?", [blogId], (error, blogData) => {
-    if (error) {
-      return console.log(error);
-    }
-    db.query("SELECT * FROM users WHERE id=?", [userId], (error, userData) => {
-      if (error) {
-        return console.log(error);
-      }
-      res.render("editPost", {
-        title: "Edit post | BlogApp",
-        blogData,
-        userData,
-      });
-    });
-  });
-});
-
-router.post("/edit/:userId/:blogId", verifyToken, (req, res) => {
-  const { title, body } = req.body;
-  const { userId, blogId } = req.params;
-
-  db.query(
-    "UPDATE posts SET title= ? , body= ? WHERE id= ?",
-    [title, body, blogId],
-    (error, results) => {
-      if (error) {
-        return console.log(error);
-      }
-      console.log("Post updated!");
-      res.redirect(`/dashboard/${userId}`);
-    }
-  );
-});
 
 module.exports = router;
